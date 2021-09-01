6d0958dde8bcad16d76a000ae41cd60e727d60cb
danyeric123 <dnagarpowers@gmail.com>
2021-09-01 15:27:31 -0400

    deployment

--- yum/settings.py
+++ yum/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG') == 'True'
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = env('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
